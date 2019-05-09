1fa518003fd1254507aad40fcedc54794036b672
Canino Developments <ian@thecommittedbug.io>
2019-05-09 14:24:26 -0400

    create withAuthentication higher order component

---
+++ src/components/Session/withAuthentication.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const withAuthentication = (Component) => {
+    class WithAuthentication extends React.Component {
+        render() {
+            return <Component {...this.props} />
+        }
+    }
+
+    return WithAuthentication;
+};
+
+export default withAuthentication;
+
