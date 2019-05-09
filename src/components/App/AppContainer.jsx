5552df36d22461468628f29763b31c34ffa5d31d
Canino Developments <ian@thecommittedbug.io>
2019-05-09 14:02:51 -0400

    establish basic privileged routes

--- src/components/App/AppContainer.jsx
+++ src/components/App/AppContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import * as ROUTES from '../../constants/routes';
 
@@ -6,6 +6,8 @@ import {
     BrowserRouter as Router,
     Route,
 } from 'react-router-dom';
+
+import { withFirebase } from '../Firebase';
 
 import Navigation from '../Navigation';
 import Landing from '../Landing';
@@ -16,20 +18,40 @@ import Home from '../Home';
 import Account from '../Account';
 import Admin from '../Admin';
 
-const App = () => (
-    <Router>
-        <Navigation />
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-        <hr />
+        this.state = {
+            authUser: null,
+        };
+    }
 
-        <Route exact path={ROUTES.LANDING} component={Landing} />
-        <Route path={ROUTES.SIGN_UP} component={SignUp} />
-        <Route path={ROUTES.SIGN_IN} component={SignIn} />
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-        <Route path={ROUTES.ACCOUNT} component={Account} />
-        <Route path={ROUTES.ADMIN} component={Admin} />
-        <Route path={ROUTES.HOME} component={Home} />
-    </Router>
-);
+    componentDidMount() {
+        this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser 
+            ? this.setState({authUser}) 
+            : this.setState({ authUser: null });
+        });
+    }
 
-export default App;
+    render () {
+        return (
+        <Router>
+            <Navigation authUser={this.state.authUser} />
+
+            <hr />
+
+            <Route exact path={ROUTES.LANDING} component={Landing} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.SIGN_IN} component={SignIn} />
+            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+            <Route path={ROUTES.ACCOUNT} component={Account} />
+            <Route path={ROUTES.ADMIN} component={Admin} />
+            <Route path={ROUTES.HOME} component={Home} />
+        </Router>
+        )
+    }
+};
+
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,12 +5,13 @@ import SignOutButton from '../SignOut';
 
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
+const Navigation = ({authUser}) => (
+<div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth = () => (
     <div>
         <ul>
-            <li>
-                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-            </li>
             <li>
                 <Link to={ROUTES.LANDING}>Landing</Link>
             </li>
@@ -29,5 +30,18 @@ const Navigation = () => (
         </ul>
     </div>
 );
+
+const NavigationNonAuth = () => (
+    <div>
+        <ul>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>    
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+        </ul>
+    </div>
+)
 
 export default Navigation;
