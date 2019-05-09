32a7b1ca68f13bee050fc67f5839cbe84835688f
Canino Developments <ian@thecommittedbug.io>
2019-05-09 13:38:39 -0400

    create Landing placeholder

---
+++ src/components/Landing/LandingContainer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class LandingContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <React.Fragment>
+                <h1>Landing</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+const Landing = LandingContainer;
+
+export default Landing;
